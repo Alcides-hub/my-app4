e8640858814dc677d6036cb1e0dd27a13b7097da
alcides_t <alcides.dml@icloud.com>
2023-06-08 16:18:02 +0900

    Throw the dice in react native

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import React, {useState} from 'react';
+import {Styles} from './styles';
+
+export default function App() {
+  const [dice, setDice] = useState(1);
+  const images={
+    pics:{
+      '1':require('./assets/dice 1.png'),
+      '2':require('./assets/dice 2.png'),
+      '3':require('./assets/dice 3.png'),
+      '4':require('./assets/dice 4.png'),
+      '5':require('./assets/dice 5.png'),
+      '6':require('./assets/dice 6.png'),
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={images.pics[dice]}/>
+      <Text style={styles.word}>Please press me</Text>
+      <Button title='Hey! Throw the dice!' style={styles.button} onPress={()=>setDice(Math.floor(Math.random()*6))}/>
+    </View>
+  );
+}
+
